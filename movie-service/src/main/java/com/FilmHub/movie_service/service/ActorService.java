011ebf0f6dacb6019bc9f98831514fc20e9e7eee
sandy <nasdrahlovejinx@hotmail.com>
2024-12-24 16:41:15 +0100

    implement exceptions to actorService

--- movie-service/src/main/java/com/FilmHub/movie_service/service/ActorService.java
+++ movie-service/src/main/java/com/FilmHub/movie_service/service/ActorService.java
@@ -1,11 +1,15 @@
 package com.FilmHub.movie_service.service;
+import com.FilmHub.movie_service.exceptions.ActorNotFoundException;
+import com.FilmHub.movie_service.exceptions.DuplicateActorException;
 import com.FilmHub.movie_service.mapper.ActorMapper;
-import com.FilmHub.movie_service.mapper.MovieMapper;
 import com.FilmHub.movie_service.models.Actor;
 import com.FilmHub.movie_service.payload.dto.ActorDTO;
 import com.FilmHub.movie_service.payload.request.ActorRequest;
 import com.FilmHub.movie_service.repository.ActorRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,36 +35,42 @@ public class ActorService {
     // Obtener actor por ID
     public ActorDTO getActorById(Long id) {
         Actor actor = actorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Actor con ID " + id + " no encontrado"));
+                .orElseThrow(() -> new ActorNotFoundException("Actor con ID " + id + " no encontrado"));
         return actorMapper.toDTO(actor);
     }
 
 
     // Crear actor
     public ActorDTO createActor(ActorRequest actorRequest) {
-        Actor actor = new Actor();
-        actor.setName(actorRequest.getName());
-
-        Actor savedActor = actorRepository.save(actor);
-
-        return actorMapper.toDTO(savedActor);
+        try {
+            Actor actor = actorMapper.toEntity(actorRequest);// Convertir ActorRequest a entidad Movie
+            Actor savedActor = actorRepository.save(actor); //guarda en BD
+            return actorMapper.toDTO(savedActor);
+        } catch (DataIntegrityViolationException ex) {
+            throw new DuplicateActorException("El actor  '" + actorRequest.getName() + "' ya existe.");
+        }
     }
 
     // Actualizar actor
     public ActorDTO updateActor(Long id, ActorRequest actorRequest) {
-        Actor actor = actorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Actor con ID " + id + " no encontrado"));
 
-        actor.setName(actorRequest.getName());
-        Actor updatedActor = actorRepository.save(actor);
+        // Verificar que existe el actor
+        Actor existingActor = actorRepository.findById(id)
+                .orElseThrow(() -> new ActorNotFoundException("Actor con ID " + id + " no encontrado"));
 
+        // Actualizar solo los campos que se proporcionan en el DTO
+        if (StringUtils.hasText(actorRequest.getName())) {
+            existingActor.setName(actorRequest.getName());
+        }
+        // Guardar el actor actualizado en el repositorio
+        Actor updatedActor = actorRepository.save(existingActor);
         return actorMapper.toDTO(updatedActor);
     }
 
     // Eliminar actor
     public void deleteActor(Long id) {
         if (!actorRepository.existsById(id)) {
-            throw new RuntimeException("Actor con ID " + id + " no encontrado");
+            throw new ActorNotFoundException("Actor con ID " + id + " no encontrado");
         }
         actorRepository.deleteById(id);
     }
